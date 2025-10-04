754b12ba0f2fb4ba39154b197c8d06344b59ac19
agdral <ezequiel.escobar@proton.me>
2025-10-04 02:30:29 -0300

    remove restart of safeeyes

--- services/safeEyes/default.nix
+++ services/safeEyes/default.nix
@@ -9,12 +9,11 @@
         Description = "safeeyes";
         After = ["graphical-session.target"];
         Requisite = ["graphical-session.target"];
-        ConditionEnvironment = ["HYPRLAND_INSTANCE_SIGNATURE"];
       };
       Service = {
         Type = "simple";
         ExecStart = "${lib.getExe pkgs.safeeyes} -e";
-        Restart = "on-failure";
+        Restart = "no";
         RestartSec = 3;
         TimeoutStopSec = 10;
       };
