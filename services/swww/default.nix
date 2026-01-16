3fae150e757e5a1c318b4dc23f15aa0b0761b786
agdral <ezequiel.escobar@proton.me>
2026-01-16 03:14:13 -0300

    change system services to exec hyprland

--- services/swww/default.nix
+++ services/swww/default.nix
@@ -4,27 +4,29 @@
     (writeShellScriptBin "imageShuffle" (builtins.readFile ./dot/imageShuffle.sh))
   ];
 
-  systemd.user.services = {
-    swww = {
-      Unit = {
-        Description = "Walker";
-        After = ["graphical-session.target"];
-        Requisite = ["graphical-session.target"];
-      };
-      Service = {
-        Type = "simple";
-        ExecStart = "${pkgs.swww}/bin/swww-daemon";
-        Restart = "on-failure";
-        RestartSec = 3;
-        TimeoutStopSec = 10;
-      };
-      Install = {
-        WantedBy = ["graphical-session.target"];
-      };
-    };
-  };
+  # systemd.user.services = {
+  #   swww = {
+  #     Unit = {
+  #       Description = "Walker";
+  #       After = ["graphical-session.target"];
+  #       Requisite = ["graphical-session.target"];
+  #       ConditionEnvironment = ["HYPRLAND_INSTANCE_SIGNATURE"];
+  #     };
+  #     Service = {
+  #       Type = "simple";
+  #       ExecStart = "${pkgs.swww}/bin/swww-daemon";
+  #       Restart = "on-failure";
+  #       RestartSec = 3;
+  #       TimeoutStopSec = 10;
+  #     };
+  #     Install = {
+  #       WantedBy = ["graphical-session.target"];
+  #     };
+  #   };
+  # };
 
   wayland.windowManager.hyprland.settings.exec-once = [
+    "swww-daemon"
     "imageShuffle $XDG_PICTURES_DIR/Wallpaper"
   ];
 }
--- services/walker/default.nix
+++ services/walker/default.nix
@@ -1,27 +1,37 @@
-{pkgs, lib, ...}: {
+{
+  pkgs,
+  # lib,
+  ...
+}: {
   home.packages = [pkgs.walker];
   home.file = {
     ".config/walker/config.toml".source = ./dot/config.toml;
     ".config/walker/themes/vault.css".source = ./dot/themes/vault.css;
     ".config/walker/themes/vault.toml".source = ./dot/themes/vault.toml;
   };
-  systemd.user.services = {
-    walker = {
-      Unit = {
-        Description = "Walker";
-        After = ["graphical-session.target"];
-        Requisite = ["graphical-session.target"];
-      };
-      Service = {
-        Type = "simple";
-        ExecStart = "${lib.getExe pkgs.walker} --gapplication-service";
-        Restart = "on-failure";
-        RestartSec = 3;
-        TimeoutStopSec = 10;
-      };
-      Install = {
-        WantedBy = ["graphical-session.target"];
-      };
-    };
-  };
+
+  # systemd.user.services = {
+  #   walker = {
+  #     Unit = {
+  #       Description = "Walker";
+  #       After = ["graphical-session.target"];
+  #       Requisite = ["graphical-session.target"];
+  #       ConditionEnvironment = ["HYPRLAND_INSTANCE_SIGNATURE"];
+  #     };
+  #     Service = {
+  #       Type = "simple";
+  #       ExecStart = "${lib.getExe pkgs.walker} --gapplication-service";
+  #       Restart = "on-failure";
+  #       RestartSec = 3;
+  #       TimeoutStopSec = 10;
+  #     };
+  #     Install = {
+  #       WantedBy = ["graphical-session.target"];
+  #     };
+  #   };
+  # };
+
+  wayland.windowManager.hyprland.settings.exec-once = [
+    "walker --gapplication-service"
+  ];
 }
