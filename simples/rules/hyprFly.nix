b9c7ab8a8acedb61042fa82b3ec2e71f7c9405a8
agdral <ezequiel.escobar@proton.me>
2026-01-28 18:56:27 -0300

    fix border size

--- simples/rules/hyprFly.nix
+++ simples/rules/hyprFly.nix
@@ -1,8 +1,7 @@
 [
   "match:class HyprFly, monitor HDMI-A-1"
-  "match:class HyprFly, noborder"
   "match:class HyprFly, rounding 0"
-  "match:class HyprFly, no_border 1"
+  "match:class HyprFly, border_size 0"
   "match:class HyprFly, no_blur 1"
   "match:class HyprFly, float 1"
   "match:class HyprFly, center 1"
