9bc6a7e42ccc088cea2d2b62ace81af4622bf4e0
agdral <ezequiel.escobar@proton.me>
2026-01-28 18:52:31 -0300

    fix rules missing

--- simples/rules/hyprFly.nix
+++ simples/rules/hyprFly.nix
@@ -2,9 +2,9 @@
   "match:class HyprFly, monitor HDMI-A-1"
   "match:class HyprFly, noborder"
   "match:class HyprFly, rounding 0"
-  "match:class HyprFly, noblur"
-  "match:class HyprFly, noanim"
-  "match:class HyprFly, float"
+  "match:class HyprFly, no_border 1"
+  "match:class HyprFly, no_blur 1"
+  "match:class HyprFly, float 1"
+  "match:class HyprFly, center 1"
   "match:class HyprFly, size 100% 100%"
-  "match:class HyprFly, center"
 ]
--- simples/rules/safeEyes.nix
+++ simples/rules/safeEyes.nix
@@ -2,20 +2,20 @@
   # SafeEyes-0
   "match:title SafeEyes-0, monitor DP-1"
   "match:title SafeEyes-0, workspace special:safe-0"
-  "match:title SafeEyes-0, noborder"
   "match:title SafeEyes-0, rounding 0"
-  "match:title SafeEyes-0, noblur"
-  "match:title SafeEyes-0, float"
+  "match:title SafeEyes-0, no_border 1"
+  "match:title SafeEyes-0, no_blur 1"
+  "match:title SafeEyes-0, float 1"
+  "match:title SafeEyes-0, center 1"
   "match:title SafeEyes-0, size 100% 100%"
-  "match:title SafeEyes-0, center"
 
   # SafeEyes-1
   "match:title SafeEyes-1, monitor HDMI-A-1"
   "match:title SafeEyes-1, workspace special:safe-1"
-  "match:title SafeEyes-1, noborder"
   "match:title SafeEyes-1, rounding 0"
-  "match:title SafeEyes-1, noblur"
-  "match:title SafeEyes-1, float"
+  "match:title SafeEyes-1, no_border 1"
+  "match:title SafeEyes-1, no_blur 1"
+  "match:title SafeEyes-1, float 1"
+  "match:title SafeEyes-1, center 1"
   "match:title SafeEyes-1, size 100% 100%"
-  "match:title SafeEyes-1, center"
 ]
