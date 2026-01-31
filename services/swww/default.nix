a9353f75e21dfd3269301e8e405cc096b7bcfd51
agdral <ezequiel.escobar@proton.me>
2026-01-31 02:53:28 -0300

    create imageshuffle service

--- services/swww/default.nix
+++ services/swww/default.nix
@@ -17,12 +17,29 @@ in {
       Service = {
         Type = "simple";
 
-        ExecStartPre = "${pkgs.swww}/bin/swww-daemon";
-        ExecStart = "${imageShuffle}/bin/imageShuffle /data/kael/Pictures/Wallpaper";
+        ExecStart = "${pkgs.swww}/bin/swww-daemon";
 
         Restart = "on-failure";
         RestartSec = 3;
         TimeoutStopSec = 10;
+      };
+      Install = {
+        WantedBy = ["graphical-session.target"];
+      };
+    };
+
+    imageShuffle = {
+      Unit = {
+        Description = "Image Shuffle Wallpaper Service";
+        After = ["graphical-session.target" "swww.service"];
+        Requisite = ["graphical-session.target"];
+        ConditionEnvironment = ["HYPRLAND_INSTANCE_SIGNATURE"];
+      };
+      Service = {
+        Type = "simple";
+        ExecStart = "${imageShuffle}/bin/imageShuffle /data/kael/Pictures/Wallpaper";
+        Restart = "on-failure";
+        RestartSec = 3;
       };
       Install = {
         WantedBy = ["graphical-session.target"];
